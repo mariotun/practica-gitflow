8c984285f0cfe5dc5a4d7e5e1f2d804293381335
mariotun <oriamnut09@gmail.com>
2022-08-01 12:44:16 -0600

    Proyecto base

---
+++ main.py
@@ -0,0 +1,7 @@
+from src import req_mario
+
+def main():
+    saludo = req_mario.Bienvenida("Proyecto base")
+    print(saludo.mostrar_Mensaje())
+
+main()
---
+++ src/req_mario.py
@@ -0,0 +1,10 @@
+
+
+class Bienvenida:
+
+    def __init__(self,mensaje):
+        self.mensaje = mensaje
+    
+    def mostrar_Mensaje(self):
+        mesaje = self.mensaje
+        return mesaje
